3e53cb48829a7452ab6cc2a35dadbcb38854b87d
Adam Legg <adam.legg@gmail.com>
2016-08-23 18:20:38 -0600

    cleaned up

---
+++ physics/lib/calcs.ex
@@ -0,0 +1,6 @@
+defmodule Calcs do
+
+  def sqaure_root(val), do: :math.sqrt
+  def squared(val), do: val * val
+  def cubed(val), do: val * val * val 
+end
--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,28 +1,13 @@
-defmodule Converter do
-  def to_nearest_tenth(val) do
-    Float.ceil(val, 1)
-  end
-
-  def to_km(velocity) do
-    velocity / 1000
-  end
-
-  def to_light_seconds({:miles, miles}) do
-    (miles * 5.36819e-6) |> round_down
-  end
-
-  def to_light_seconds({:meters, meters}) do
-    (meters * 3.335638620368e-9) |> round_down
-  end
-
-  def round_down(val) when is_float(val), do: trunc(val)
-end
-
 defmodule Physics.Rocketry do
-  def escape_velocity(:earth) do
-    %{mass: 5.972e24, radius: 6.371e6}
-      |> escape_velocity
-  end
+  import Converter
+  import Calcs
+  import Physics.Laws
+  import Planets
+
+  def escape_velocity(:earth) do earth |> escape_velocity end
+  def escape_velocity(:moon) do moon |> escape_velocity end
+  def escape_velocity(:mars) do mars |> escape_velocity end
+
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
@@ -31,7 +16,6 @@ defmodule Physics.Rocketry do
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    newtons_constant = 6.67e-11
     2 * newtons_constant * mass / radius
       |> :math.sqrt
   end
